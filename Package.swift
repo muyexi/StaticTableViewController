a20cda3e0fcdb20899b2e104199fa8bc375f1c76
muyexi <muyexi@gmail.com>
2020-04-13 11:16:43 +0800

    fix(SPM): Package Resolution Failed
    https://github.com/muyexi/StaticTableViewController/issues/5#issuecomment-612686263

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "StaticTableViewController"
+    name: "StaticTableViewController",
+	platforms: [
+		.iOS(.v8)
+	],
+    products: [
+        .library(name: "StaticTableViewController", targets: ["StaticTableViewController"]),
+    ],
+    targets: [
+        .target(
+            name: "StaticTableViewController",
+            path: "Sources"
+        )
+    ]    
 )
